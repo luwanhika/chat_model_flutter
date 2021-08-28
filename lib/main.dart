16aa0812e3a8d4b68790345e00174d633ab55303
Luwa <luwanhika@hotmail.com>
2021-08-28 02:06:16 -0300

    create a chat_Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
+import 'package:chat/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Luwa',
+      title: 'Chat Luwa',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
+        iconTheme: IconThemeData(
+          color: Colors.deepOrange,
+        ),
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
